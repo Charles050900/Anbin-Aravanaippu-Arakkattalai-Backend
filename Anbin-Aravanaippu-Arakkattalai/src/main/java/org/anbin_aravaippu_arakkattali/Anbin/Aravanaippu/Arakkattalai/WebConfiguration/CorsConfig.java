76dbfc24792a1149494e3a8078dc6fc0c7b594a2
Charles050900 <charlesbellarmin05@gmail.com>
2025-10-07 10:47:02 +0530

    {commit_message}

---
+++ Anbin-Aravanaippu-Arakkattalai/src/main/java/org/anbin_aravaippu_arakkattali/Anbin/Aravanaippu/Arakkattalai/WebConfiguration/CorsConfig.java
@@ -0,0 +1,34 @@
+package org.anbin_aravaippu_arakkattali.Anbin.Aravanaippu.Arakkattalai.WebConfiguration;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class CorsConfig {
+
+    // Update with your deployed frontend URL
+    private static final String LOCAL_FRONTEND = "http://localhost:5173";
+    private static final String DEPLOYED_FRONTEND = "https://charles050900.github.io/Anbin-Aravanaippu-Arakkattalai/";
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(LOCAL_FRONTEND, DEPLOYED_FRONTEND)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
+
+
